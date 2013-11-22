ca5b6ab609dd7707024bf9e69245f435d5e3a456
vaskou <vaskou@hotmail.com>
2013-11-22 14:15:48 +0200

    Update 22/11/2013 14:15

---
+++ alfacare theme/js/addons/my_changes/mcs_alpha_grid.js
@@ -0,0 +1,27 @@
+$(function(){
+	
+	var flag=false;
+	
+	initBindings();
+	
+	$(document).ajaxStop(function() {
+        if(flag){
+			initBindings();
+		}
+		flag=true;
+    });
+	
+	function initBindings(){
+		$(".mcs_alpha_grid .product").hover(
+		function(){
+			$(this).find('.price-wrapper').animate({backgroundColor:"#e6e6e6"}, 500);
+			$(this).find('.product_extra_info').animate({backgroundColor:"#e6e6e6"}, 500);
+			$(this).find('.product-meta-wrapper').animate({backgroundColor:"#e6e6e6"}, 500);
+		},
+		function(){
+			$(this).find('.price-wrapper').animate({backgroundColor:"#fff"}, 100);
+			$(this).find('.product_extra_info').animate({backgroundColor:"#fff"}, 100);
+			$(this).find('.product-meta-wrapper').animate({backgroundColor:"#fff"}, 100);
+		});
+	}
+});
