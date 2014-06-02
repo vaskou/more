ddc7bd60e491be95e499618e298ce2002ff56bb6
vaskou <vaskou@hotmail.com>
2014-06-02 15:49:36 +0300

    Update 02/06/2014 15:50

--- pluslight2/js/theme_scripts/pluslight/mcs_product_page_height.js
+++ pluslight2/js/theme_scripts/pluslight/mcs_product_page_height.js
@@ -1,5 +1,4 @@
 $(function(){
 	image_panel_height=$(".product-main-info .image-wrap").outerHeight(true);
-	console.log(image_panel_height);
 	$(".product-main-info .product-info").css({"min-height":image_panel_height+"px"});
 });
