4f8861c319897dea9925c4364acaedb786e4c151
vaskou <vaskou@hotmail.com>
2014-06-24 16:32:14 +0300

    Update 24/06/2014 14:32
    -Updated mcs_child_shop (not finished)

--- mcs_child_shop/app/addons/mcs_child_shop/func.php
+++ mcs_child_shop/app/addons/mcs_child_shop/func.php
@@ -25,7 +25,8 @@ function fn_mcs_sync_product($product_id)
 	$prices=fn_mcs_get_prices($product_id);
 	// Sync options
 	$options=fn_mcs_get_product_options($product_id);
-	
+	// Sync features
+	$features=fn_mcs_get_product_features($product_id);
 	
 	fn_mcs_db_connect_child();
 	// Sync main data
@@ -38,7 +39,8 @@ function fn_mcs_sync_product($product_id)
 	fn_mcs_put_product_categories($main_data,$pid);
 	// Sync options
 	fn_mcs_put_product_options($options,$product_id);
-
+	// Sync features
+	fn_mcs_put_product_features($features,$product_id);
 
 /*    // Clone product features
     $data = db_get_array("SELECT * FROM ?:product_features_values WHERE product_id = ?i", $product_id);
@@ -78,8 +80,8 @@ function fn_mcs_sync_product($product_id)
 function fn_mcs_get_parent_main_data($product_id)
 {
 	$data = db_get_row("SELECT * FROM ?:products WHERE product_id = ?i", $product_id);
-	$data['product_cid']=$data['product_id'];
-    unset($data['product_id']);
+/*	$data['product_cid']=$data['product_id'];
+    unset($data['product_id']);*/
     $data['status'] = 'D';
     $data['timestamp'] = $data['updated_timestamp'] = time();
 	
@@ -88,7 +90,6 @@ function fn_mcs_get_parent_main_data($product_id)
 
 function fn_mcs_put_parent_main_data($data)
 {
-	//$pid = db_query("INSERT INTO ?:products ?e", $data);
 	$pid=db_replace_into("products", $data);
 	
 	return $pid;
@@ -104,7 +105,6 @@ function fn_mcs_put_descriptions($data, $pid)
 {
 	foreach ($data as $v) {
         $v['product_id'] = $pid;
-    //    db_query("INSERT INTO ?:product_descriptions ?e", $v);
 		db_replace_into("product_descriptions", $v);
     }
 }
@@ -120,7 +120,6 @@ function fn_mcs_put_prices($data, $pid)
 	foreach ($data as $v) {
         $v['product_id'] = $pid;
         unset($v['price_id']);
-    //    db_query("INSERT INTO ?:product_prices ?e", $v);
 		db_replace_into("product_prices", $v);
     }
 }
@@ -172,31 +171,70 @@ function fn_mcs_get_all_product_options()
 function fn_mcs_get_product_options($product_id)
 {
 	$product_global_option_links=db_get_array("SELECT * FROM ?:product_global_option_links WHERE product_id=$product_id");
-	$product_options=db_get_array("SELECT * FROM ?:product_options WHERE product_id=$product_id");
+	$product_options=array();
+	$product_options_descriptions=array();
+	$product_option_variants=array();
+	$product_option_variants_descriptions=array();
+	if(!empty($product_global_option_links)){
+		foreach($product_global_option_links as $k=>$v){
+			$option_id=$v['option_id'];
+			fn_mcs_get_product_options_and_descriptions($option_id,$product_options,$product_options_descriptions);
+			fn_mcs_get_product_option_variants_and_descriptions($option_id,$product_option_variants,$product_option_variants_descriptions);
+		}
+	}else{
+		$option_ids=db_get_array("SELECT option_id FROM ?:product_options WHERE product_id=$product_id");
+		foreach($option_ids as $k=>$v){
+			$option_id=$v['option_id'];			
+			fn_mcs_get_product_options_and_descriptions($option_id,$product_options,$product_options_descriptions);
+			fn_mcs_get_product_option_variants_and_descriptions($option_id,$product_option_variants,$product_option_variants_descriptions);
+		}
+	}
 	$product_options_exceptions=db_get_array("SELECT * FROM ?:product_options_exceptions WHERE product_id=$product_id");
 	$product_options_inventory=db_get_array("SELECT * FROM ?:product_options_inventory WHERE product_id=$product_id");
 	
 	$data=array(
 		'product_global_option_links'=>$product_global_option_links,
 		'product_options'=>$product_options,
+		'product_options_descriptions'=>$product_options_descriptions,
 		'product_options_exceptions'=>$product_options_exceptions,
 		'product_options_inventory'=>$product_options_inventory,
+		'product_option_variants'=>$product_option_variants,
+		'product_option_variants_descriptions'=>$product_option_variants_descriptions
 	);
 	
 	return $data;
 }
 
+function fn_mcs_get_product_options_and_descriptions($option_id,&$product_options,&$product_options_descriptions)
+{
+	$temp_options=db_get_array("SELECT * FROM ?:product_options WHERE option_id=$option_id");
+	$temp_options_descriptions=db_get_array("SELECT * FROM ?:product_options_descriptions WHERE option_id=$option_id");
+	
+	array_push($product_options,$temp_options[0]);
+	array_push($product_options_descriptions,$temp_options_descriptions[0]);
+}
+
+function fn_mcs_get_product_option_variants_and_descriptions($option_id,&$product_option_variants,&$product_option_variants_descriptions)
+{
+	$temp_option_variants=db_get_array("SELECT * FROM ?:product_option_variants WHERE option_id=$option_id");
+	$temp_option_variants_descriptions=array();
+	foreach($temp_option_variants as $k1=>$v1){
+		$variant_id=$v1['variant_id'];
+		$data=db_get_array("SELECT * FROM ?:product_option_variants_descriptions WHERE variant_id=$variant_id");
+		array_push($temp_option_variants_descriptions,$data[0]);
+	}
+	
+	foreach($temp_option_variants as $k2=>$v2){
+		array_push($product_option_variants,$v2);
+	}
+	foreach($temp_option_variants_descriptions as $k2=>$v2){
+		array_push($product_option_variants_descriptions,$v2);
+	}
+}
+
 function fn_mcs_put_all_product_options($data)
 {
 	try{
-		$data=fn_mcs_match_all_options_product_id($data);
-		/*fn_mcs_multi_db_replace_into("product_global_option_links", $data['product_global_option_links']);
-		fn_mcs_multi_db_replace_into("product_options", $data['product_options']);
-		fn_mcs_multi_db_replace_into("product_options_descriptions", $data['product_options_descriptions']);
-		fn_mcs_multi_db_replace_into("product_options_exceptions", $data['product_options_exceptions']);
-		fn_mcs_multi_db_replace_into("product_options_inventory", $data['product_options_inventory']);
-		fn_mcs_multi_db_replace_into("product_option_variants", $data['product_option_variants']);
-		fn_mcs_multi_db_replace_into("product_option_variants_descriptions", $data['product_option_variants_descriptions']);*/
 		foreach($data as $k=>$v){
 			fn_mcs_multi_db_replace_into($k,$v);
 		}
@@ -211,68 +249,9 @@ function fn_mcs_put_all_product_options($data)
 
 function fn_mcs_put_product_options($data,$product_id)
 {
-	$data=fn_mcs_match_options_product_id($data,$product_id);
 	foreach($data as $k=>$v){
 		fn_mcs_multi_db_replace_into($k,$v);
 	}
-}
-
-function fn_mcs_match_all_options_product_id($data)
-{
-	$mapped_products_ids=db_get_array("SELECT product_id, product_cid FROM ?:products");
-	foreach($mapped_products_ids as $k=>$v){
-		foreach($data['product_global_option_links'] as $k1=>$v1){
-			if($v['product_cid']==$v1['product_id']){
-				$data['product_global_option_links'][$k1]['product_id']=$v['product_id'];
-			}
-		}
-		foreach($data['product_options'] as $k2=>$v2){
-			if($v['product_cid']==$v2['product_id']){
-				$data['product_options'][$k2]['product_id']=$v['product_id'];
-			}
-		}
-		foreach($data['product_options_exceptions'] as $k3=>$v3){
-			if($v['product_cid']==$v3['product_id']){
-				$data['product_options_exceptions'][$k3]['product_id']=$v['product_id'];
-			}
-		}
-		foreach($data['product_options_inventory'] as $k4=>$v4){
-			if($v['product_cid']==$v4['product_id']){
-				$data['product_options_inventory'][$k4]['product_id']=$v['product_id'];
-			}
-		}
-	}
-	
-	return $data;
-}
-
-function fn_mcs_match_options_product_id($data,$product_id)
-{
-	$mapped_products_ids=db_get_array("SELECT product_id, product_cid FROM ?:products WHERE product_cid=$product_id");
-	foreach($mapped_products_ids as $k=>$v){
-		foreach($data['product_global_option_links'] as $k1=>$v1){
-			if($v['product_cid']==$v1['product_id']){
-				$data['product_global_option_links'][$k1]['product_id']=$v['product_id'];
-			}
-		}
-		foreach($data['product_options'] as $k2=>$v2){
-			if($v['product_cid']==$v2['product_id']){
-				$data['product_options'][$k2]['product_id']=$v['product_id'];
-			}
-		}
-		foreach($data['product_options_exceptions'] as $k3=>$v3){
-			if($v['product_cid']==$v3['product_id']){
-				$data['product_options_exceptions'][$k3]['product_id']=$v['product_id'];
-			}
-		}
-		foreach($data['product_options_inventory'] as $k4=>$v4){
-			if($v['product_cid']==$v4['product_id']){
-				$data['product_options_inventory'][$k4]['product_id']=$v['product_id'];
-			}
-		}
-	}
-	
-	return $data;
 }
 
 function fn_mcs_get_all_product_features()
@@ -294,10 +273,47 @@ function fn_mcs_get_all_product_features()
 	return $data;
 }
 
+function fn_mcs_get_product_features($product_id)
+{
+	$product_features_values=db_get_array("SELECT * FROM ?:product_features_values WHERE product_id=$product_id");
+	$product_features=array();
+	$product_features_descriptions=array();
+	$product_feature_variants=array();
+	$product_feature_variant_descriptions=array();
+	
+	foreach($product_features_values as $k=>$v){
+		$feature_id=$v['feature_id'];
+		
+		$temp_product_features=db_get_array("SELECT feature_id, feature_code, company_id, feature_type, parent_id, display_on_product, display_on_catalog, display_on_header, status, position, comparison FROM ?:product_features WHERE feature_id=$feature_id");
+		array_push($product_features,$temp_product_features[0]);
+		
+		$temp_product_features_descriptions=db_get_array("SELECT * FROM ?:product_features_descriptions WHERE feature_id=$feature_id");
+		array_push($product_features_descriptions,$temp_product_features_descriptions[0]);
+		
+		$variant_id=$v['variant_id'];
+		
+		$temp_product_feature_variants=db_get_array("SELECT * FROM ?:product_feature_variants WHERE variant_id=$variant_id");
+		array_push($product_feature_variants,$temp_product_feature_variants[0]);
+		
+		$temp_product_feature_variant_descriptions=db_get_array("SELECT * FROM ?:product_feature_variant_descriptions WHERE variant_id=$variant_id");
+		array_push($product_feature_variant_descriptions,$temp_product_feature_variant_descriptions[0]);
+
+	}
+	
+	$data=array(
+		'product_features'=>$product_features,
+		'product_features_descriptions'=>$product_features_descriptions,
+		'product_features_values'=>$product_features_values,
+		'product_feature_variants'=>$product_feature_variants,
+		'product_feature_variant_descriptions'=>$product_feature_variant_descriptions
+	);
+	
+	return $data;;
+}
+
 function fn_mcs_put_all_product_features($data)
 {
 	try{
-		$data=fn_mcs_match_all_product_features($data);
 		foreach($data as $k=>$v){
 			fn_mcs_multi_db_replace_into($k,$v);
 		}
@@ -310,23 +326,10 @@ function fn_mcs_put_all_product_features($data)
 	}
 }
 
-function fn_mcs_match_all_product_features($data)
+function fn_mcs_put_product_features($data,$product_id)
 {
-	try{
-		$mapped_products_ids=db_get_array("SELECT product_id, product_cid FROM ?:products");
-		foreach($mapped_products_ids as $k=>$v){
-			foreach($data['product_features_values'] as $k1=>$v1){
-				if($v['product_cid']==$v1['product_id']){
-					$data['product_features_values'][$k1]['product_id']=$v['product_id'];
-				}
-			}
-		}
-	
-		return $data;
-	}
-	catch(Exception $e){
-		echo 'Caught exception: '.$e->getMessage()."\n";
-		return false;
+	foreach($data as $k=>$v){
+		fn_mcs_multi_db_replace_into($k,$v);
 	}
 }
 
