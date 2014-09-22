fe2969c59f0ce36805dff4b5658f330fe8531aad
vaskou <vaskou@hotmail.com>
2014-09-22 15:48:10 +0300

    Update 22/09/2014 15:48

--- mcs_addons/app/addons/mcs_font_icons/schemas/settings/variants.functions.post.php
+++ mcs_addons/app/addons/mcs_font_icons/schemas/settings/variants.functions.post.php
@@ -1,4 +1,5 @@
 <?php
+use Tygh\Registry;
 
 if ( !defined('AREA') ) { die('Access denied'); }
 
@@ -6,7 +7,8 @@ function fn_settings_variants_addons_mcs_font_icons_mcs_icons_library()
 {
 	$files_array=array();
 	$extensions=array('css','less');
-	$dir_path="design/themes/responsive/css/addons/mcs_font_icons/libraries";
+	$theme_name=Registry::get('settings.theme_name');
+	$dir_path= fn_get_theme_path('[themes]/[theme]/css', 'C') . '/addons/mcs_font_icons/libraries';
 	$files=fn_get_dir_contents($dir_path,false,true,$extensions,'',true);
 	foreach($files as $file){
 		$key=str_replace(".","_DOT_",$file);
@@ -20,7 +22,8 @@ function fn_settings_variants_addons_mcs_font_icons_mcs_custom_icons()
 {
 	$files_array=array();
 	$extensions=array('css','less');
-	$dir_path="design/themes/responsive/css/addons/mcs_font_icons/custom";
+	$theme_name=Registry::get('settings.theme_name');
+	$dir_path= fn_get_theme_path('[themes]/[theme]/css', 'C') . '/addons/mcs_font_icons/custom';
 	$files=fn_get_dir_contents($dir_path,false,true,$extensions,'',true);
 	foreach($files as $file){
 		$key=str_replace(".","_DOT_",$file);
